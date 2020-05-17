27271d2ea6544e87b8f427fd13b4d08561a35998
carsten <cr@tagion.org>
2020-05-17 11:46:29 +0300

    Creating make flow

---
+++ setup.mk
@@ -0,0 +1,40 @@
+REPOROOT?=${shell git root}
+-include $(REPOROOT)/localsetup.mk
+
+ifndef NOUNITTEST
+DCFLAGS+=-unittest
+DCFLAGS+=-g
+DCFLAGS+=$(DEBUG)
+endif
+
+DCFLAGS+=$(DIP1000) # Should support scope c= new C; // is(C == class)
+DCFLAGS+=$(DIP25)
+
+SCRIPTROOT:=${REPOROOT}/scripts/
+
+WAVMROOT:=${REPOROOT}/../WAVM/
+# WAVM C-header file
+WAVM_H:=${WAVMROOT}/Include/WAVM/wavm-c/wavm-c.h
+WAVM_DI_ROOT:=../../../wavm/
+WAVM_DI:=wavm/c/wavm.di
+WAVM_PACKAGE:=wavm.c
+# Change c-array to pointer
+WAVMa2p:=${SCRIPTROOT}/wasm_array2pointer.pl
+
+WAYS+=$(WAVM_DI_ROOT)
+
+LIBNAME:=libwavm.a
+
+# DDOC Configuration
+#
+-include ddoc.mk
+
+BIN:=$(REPOROOT)/bin/
+BUILD?=$(REPOROOT)/build
+
+WAYS+=${BIN}
+WAYS+=${BUILD}
+
+SOURCE:=tagion
+
+-include dstep.mk
