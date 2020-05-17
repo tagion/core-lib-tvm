6aea7cfc28ba1664a646f427de058471d9c603ea
carsten <cr@tagion.org>
2020-05-17 11:55:28 +0300

    Dstep make flow added to generate wavm.di

--- setup.mk
+++ setup.mk
@@ -15,8 +15,8 @@ SCRIPTROOT:=${REPOROOT}/scripts/
 WAVMROOT:=${REPOROOT}/../WAVM/
 # WAVM C-header file
 WAVM_H:=${WAVMROOT}/Include/WAVM/wavm-c/wavm-c.h
-WAVM_DI_ROOT:=../../../wavm/
-WAVM_DI:=wavm/c/wavm.di
+WAVM_DI_ROOT:=$(REPOROOT)/tagion/vm/wavm/c/
+WAVM_DI:=$(WAVM_DI_ROOT)/wavm.di
 WAVM_PACKAGE:=wavm.c
 # Change c-array to pointer
 WAVMa2p:=${SCRIPTROOT}/wasm_array2pointer.pl
