205efb3465f2be5cba9ebca6bc2a3cd073ff800a
Chikus <aztlan_abe@hotmail.com>
2020-12-28 15:49:28 +0200

    Creating wasm file for return a structure pointer.

---
+++ tests/chikustest/anytype/appwasm/testapp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct S {
+    int x;
+    double f;
+} S_t;
+
+
+S_t* func1() {
+    S_t* ret;
+    ret=malloc(sizeof(S_t));
+    ret->x=10;
+    ret->f=42.42;
+    return ret;
+}
+
+void func2(S_t* s) {
+    s->x=12;
+}
