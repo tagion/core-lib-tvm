536e0f23afe9615e5c6fdb0b0938bf77fae173a0
cbleser <cr@tagion.org>
2021-08-26 09:00:16 +0300

    WIP: Added TVMError set error type

--- tagion/tvm/wamr/TVMBasic.d
+++ tagion/tvm/wamr/TVMBasic.d
@@ -5,8 +5,8 @@ import std.meta : AliasSeq, anySatisfy, ApplyLeft, staticIndexOf;
 import std.traits : isIntegral, Unqual;
 import tagion.basic.Basic : isOneOf, isEqual;
 
-alias WasmTypes=AliasSeq!(int, long, float, double, uint, ulong, short, ushort, byte, ubyte, WasmType);
-
+alias WasmTypes = AliasSeq!(int, long, float, double, uint, ulong, short,
+        ushort, byte, ubyte, WasmType);
 
 //pragma(msg, ApplyLeft!(Unqual, int, const(ubyte)));
 
@@ -19,23 +19,25 @@ static unittest {
     static assert(isWasmType!string);
     static assert(allSatisfy!(isWasmType, double, int, ubyte));
 }
-@safe @nogc
-union WasmType {
+
+@safe @nogc union WasmType {
     import std.format;
+
     @(Types.I32) int i32;
     @(Types.I64) long i64;
     @(Types.F32) float f32;
     @(Types.F64) double f64;
-    alias WasmT=typeof(this.tupleof);
+    alias WasmT = typeof(this.tupleof);
     pure nothrow {
-        void opAssign(T)(const T x) if(isOneOf!(T, WasmT)) {
-            enum index=staticIndexOf!(T, WasmT);
+        void opAssign(T)(const T x) if (isOneOf!(T, WasmT)) {
+            enum index = staticIndexOf!(T, WasmT);
             this.tupleof[index] = x;
         }
 
         void opAssign(const uint x) {
             i32 = x;
         }
+
         void opAssign(const ulong x) {
             i64 = x;
         }
@@ -48,61 +50,59 @@ union WasmType {
             f64 = x;
         }
 
-        void opOpAssign(string op, T)(const T x) if(isOneOf!(T, WasmT)) {
-            enum index=staticIndexOf!(T, WasmT);
-            enum code=format(q{this.tupleof[index] %s= x;}, op);
+        void opOpAssign(string op, T)(const T x) if (isOneOf!(T, WasmT)) {
+            enum index = staticIndexOf!(T, WasmT);
+            enum code = format(q{this.tupleof[index] %s= x;}, op);
             mixin(code);
         }
 
-        void opOpAssign(string op, T)(const T x) if(isIntegral!T && !isOneOf!(T, WasmT)) {
+        void opOpAssign(string op, T)(const T x)
+                if (isIntegral!T && !isOneOf!(T, WasmT)) {
             static if (T.sizeof <= int.sizeof) {
-                alias U=int;
+                alias U = int;
             }
             else {
-                alias U=long;
+                alias U = long;
             }
-            enum index=staticIndexOf!(U, WasmT);
-            enum code=format!q{this.tupleof[index] %s= cast(U)x;}(op);
+            enum index = staticIndexOf!(U, WasmT);
+            enum code = format!q{this.tupleof[index] %s= cast(U)x;}(op);
             mixin(code);
         }
 
-
-
-
-            // void opOpAssign(T, string op)(const T x) if(is(T==WasmType)) {
-            //     pragma(msg, "WasmType ", T.stringof);
-            //     // enum index=staticIndexOf!(T, WasmT);
-            //     // enum code=format(q{this.tupleof[index] %s= x}, op);
-            //     // mixin(code);
-            // }
-
+        // void opOpAssign(T, string op)(const T x) if(is(T==WasmType)) {
+        //     pragma(msg, "WasmType ", T.stringof);
+        //     // enum index=staticIndexOf!(T, WasmT);
+        //     // enum code=format(q{this.tupleof[index] %s= x}, op);
+        //     // mixin(code);
+        // }
 
         const(T) get(T)() const pure nothrow if (isOneOf!(T, WasmT)) {
-            enum index=staticIndexOf!(T, WasmT);
+            enum index = staticIndexOf!(T, WasmT);
             return this.tupleof[index];
         }
 
-        const(T) get(T)() const pure  nothrow if (isIntegral!T && !isOneOf!(T, WasmT)) {
+        const(T) get(T)() const pure nothrow 
+                if (isIntegral!T && !isOneOf!(T, WasmT)) {
             static if (T.sizeof <= int.sizeof) {
-                return cast(T)i32;
+                return cast(T) i32;
             }
             else {
-                return cast(T)i64;
+                return cast(T) i64;
             }
         }
     }
 }
 
-@nogc @safe
-struct FunctionInstance {
+@nogc @safe struct FunctionInstance {
     union {
         struct {
-            uint ip;     // Bincode instruction pointer
-            ushort local_size;     /// local variable count, 0 for import function
+            uint ip; // Bincode instruction pointer
+            ushort local_size; /// local variable count, 0 for import function
         }
 
     }
-    ushort param_count;     /// parameter count
+
+    ushort param_count; /// parameter count
     /+
     /* cell num of parameters */
     uint16 param_cell_num;
@@ -112,29 +112,31 @@ struct FunctionInstance {
     uint16 local_cell_num;
     +/
     /* whether it is import function or WASM function */
-    bool is_import_func;     /// whether it is import function or WASM function
+    bool is_import_func; /// whether it is import function or WASM function
 
     bool isLocalFunc() pure const nothrow {
         return !is_import_func;
     }
-    version(none) {
-    version(WASM_ENABLE_FAST_INTERP) {
-    /* cell num of consts */
-    uint16 const_cell_num;
-    }
-    uint16 *local_offsets;
-    /* parameter types */
-    uint8 *param_types;
-    /* local types, NULL for import function */
-    uint8 *local_types;
-    union U {
-        WASMFunctionImport *func_import;
-        WASMFunction *func;
-    }
-    U u;
-    version(WASM_ENABLE_MULTI_MODULE) {
-        WASMModuleInstance *import_module_inst;
-        WASMFunctionInstance *import_func_inst;
-    }
+
+    version (none) {
+        version (WASM_ENABLE_FAST_INTERP) {
+            /* cell num of consts */
+            uint16 const_cell_num;
+        }
+        uint16* local_offsets;
+        /* parameter types */
+        uint8* param_types;
+        /* local types, NULL for import function */
+        uint8* local_types;
+        union U {
+            WASMFunctionImport* func_import;
+            WASMFunction* func;
+        }
+
+        U u;
+        version (WASM_ENABLE_MULTI_MODULE) {
+            WASMModuleInstance* import_module_inst;
+            WASMFunctionInstance* import_func_inst;
+        }
     }
 }
--- tagion/tvm/wamr/TVMExtOpcode.d
+++ tagion/tvm/wamr/TVMExtOpcode.d
@@ -1,6 +1,6 @@
 module tagion.tvm.wamr.TVMExtOpcode;
 
-import WasmBase=tagion.wasm.WasmBase;
+import WasmBase = tagion.wasm.WasmBase;
 import std.algorithm.searching : canFind;
 import std.format;
 import std.array : join;
@@ -8,72 +8,75 @@ import std.traits : EnumMembers;
 
 protected {
     enum ExtraIR : ubyte {
-        ERROR                = WasmBase.IR.F64_REINTERPRET_I64+1, /// Extra jump label to handle errors
-            EXTRA_BR, /// This branch jump is used internal buy the interpreter
-            EXTERNAL_CALL, /// Call an external function from the import list
-            I32_TRUNC_SAT_F32_S,
-            I32_TRUNC_SAT_F32_U,
-            I32_TRUNC_SAT_F64_S,
-            I32_TRUNC_SAT_F64_U,
-            I64_TRUNC_SAT_F32_S,
-            I64_TRUNC_SAT_F32_U,
-            I64_TRUNC_SAT_F64_S,
-            I64_TRUNC_SAT_F64_U,
-            UNDEFINED,
-            }
+        ERROR = WasmBase.IR.F64_REINTERPRET_I64 + 1, /// Extra jump label to handle errors
+        EXTRA_BR, /// This branch jump is used internal buy the interpreter
+        EXTERNAL_CALL, /// Call an external function from the import list
+        I32_TRUNC_SAT_F32_S,
+        I32_TRUNC_SAT_F32_U,
+        I32_TRUNC_SAT_F64_S,
+        I32_TRUNC_SAT_F64_U,
+        I64_TRUNC_SAT_F32_S,
+        I64_TRUNC_SAT_F32_U,
+        I64_TRUNC_SAT_F64_S,
+        I64_TRUNC_SAT_F64_U,
+        UNDEFINED,
+    }
 }
 
 protected string generateExtendedIR(string enum_name)() {
     string[] codes;
-    codes~=format!`enum %s : ubyte {`(enum_name);
+    codes ~= format!`enum %s : ubyte {`(enum_name);
     import tagion.wasm.WasmBase : IR;
+
     enum Eliminate = [IR.NOP, IR.IF, IR.ELSE, IR.BLOCK, IR.END, IR.LOOP];
-    foreach(E; EnumMembers!(IR)) {
+    foreach (E; EnumMembers!(IR)) {
         static if (!Eliminate.canFind(E) && E !is IR.TRUNC_SAT) {
-            version(COMPACT_EXTENDED_IR) {
-                codes~=format!q{%s,}(E);
+            version (COMPACT_EXTENDED_IR) {
+                codes ~= format!q{%s,}(E);
             }
 
             else {
-                codes~=format!q{%s = %d,}(E, E);
+                codes ~= format!q{%s = %d,}(E, E);
             }
         }
     }
-    foreach(E; EnumMembers!(ExtraIR)) {
-        version(COMPACT_EXTENDED_IR) {
-            codes~=format!q{%s,}(E);
+    foreach (E; EnumMembers!(ExtraIR)) {
+        version (COMPACT_EXTENDED_IR) {
+            codes ~= format!q{%s,}(E);
         }
         else {
-            codes~=format!q{%s = %d,}(E, E);
+            codes ~= format!q{%s = %d,}(E, E);
         }
     }
-    codes~=`};`;
+    codes ~= `};`;
     return codes.join("\n");
 }
 
 // pragma(msg, generateExtendedIR);
-pragma(msg, generateExtendedIR!q{ExtendedIR});
+//pragma(msg, generateExtendedIR!q{ExtendedIR});
 mixin(generateExtendedIR!q{ExtendedIR});
 
-version(COMPACT_EXTENDED_IR) {
-protected ubyte[ubyte.max+1] generateExtendedIRToIR() {
-    ubyte[ubyte.max+1] table = ubyte.max;
+version (COMPACT_EXTENDED_IR) {
+    protected ubyte[ubyte.max + 1] generateExtendedIRToIR() {
+        ubyte[ubyte.max + 1] table = ubyte.max;
 
-    foreach(i, E; [EnumMembers!(WasmBase.IR)]) {
-        table[i] = cast(ubyte)E;
+        foreach (i, E; [EnumMembers!(WasmBase.IR)]) {
+            table[i] = cast(ubyte) E;
+        }
+        return table;
     }
-    return table;
-}
-protected ubyte[ubyte.max+1] generateIRToExtendedIR() {
-    ubyte[ubyte.max+1] table = ubyte.max;
 
-    foreach(i, E; [EnumMembers!(ExtendedIR)]) {
-        table[i] = cast(ubyte)E;
+    protected ubyte[ubyte.max + 1] generateIRToExtendedIR() {
+        ubyte[ubyte.max + 1] table = ubyte.max;
+
+        foreach (i, E; [EnumMembers!(ExtendedIR)]) {
+            table[i] = cast(ubyte) E;
+        }
+        return table;
     }
-    return table;
-}
-enum ExtendedIRToIR= generateExtendedIRToIR;
-enum IRToExtendedIR  = generateIRToExtendedIR;
+
+    enum ExtendedIRToIR = generateExtendedIRToIR;
+    enum IRToExtendedIR = generateIRToExtendedIR;
 }
 
 bool isPrefixIR(const ExtendedIR ir) pure nothrow @safe {
@@ -81,20 +84,20 @@ bool isPrefixIR(const ExtendedIR ir) pure nothrow @safe {
 }
 
 WasmBase.IR convert(const ExtendedIR ir) @safe pure nothrow {
-    version(COMPACT_EXTENDED_IR) {
+    version (COMPACT_EXTENDED_IR) {
         if (isPrefixIR(ir)) {
-            return cast(WasmBase.IR)ubyte.max;
+            return cast(WasmBase.IR) ubyte.max;
         }
-        return cast(WasmBase.IR)ExtendedIRToIR[ir];
+        return cast(WasmBase.IR) ExtendedIRToIR[ir];
     }
     else {
-        switch(ir) {
-            static foreach(E; EnumMembers!(ExtendedIR)) {
-            case E:
-                return cast(WasmBase.IR)ir;
+        switch (ir) {
+            static foreach (E; EnumMembers!(ExtendedIR)) {
+        case E:
+                return cast(WasmBase.IR) ir;
             }
-            default:
-                return cast(WasmBase.IR)ubyte.max;
+        default:
+            return cast(WasmBase.IR) ubyte.max;
         }
     }
 
@@ -102,26 +105,26 @@ WasmBase.IR convert(const ExtendedIR ir) @safe pure nothrow {
 
 @safe unittest {
     assert(Extended.IF.convert is WasmBase.IR.IF);
-    assert(Extended.ERROR is cast(WasmBase.IR)ubyte.max);
+    assert(Extended.ERROR is cast(WasmBase.IR) ubyte.max);
 }
 
 ExtendedIR convert(const WasmBase.IR ir) @safe pure nothrow {
-    version(COMPACT_EXTENDED_IR) {
+    version (COMPACT_EXTENDED_IR) {
         if (ir is WasmBase.IR.TRUNC_SAT) {
             return ExtendedIR.ERROR; // TRUNC_SAT is a IR with prefix
         }
         else {
-            return cast(ExtendedIR)IRToExtendedIR[ir];
+            return cast(ExtendedIR) IRToExtendedIR[ir];
         }
     }
     else {
-        switch(ir) {
-            static foreach(E; EnumMembers!(ExtraIR)) {
-            case E:
-                return cast(WasmBase.IR)ubyte.max;
+        switch (ir) {
+            static foreach (E; EnumMembers!(ExtraIR)) {
+        case E:
+                return cast(WasmBase.IR) ubyte.max;
             }
-            default:
-                return cast(WasmBase.IR)ir;
+        default:
+            return cast(WasmBase.IR) ir;
         }
     }
 
@@ -130,7 +133,7 @@ ExtendedIR convert(const WasmBase.IR ir) @safe pure nothrow {
 ExtendedIR convert(const WasmBase.IR ir, const ubyte suffix_ir) @safe pure nothrow {
     if (ir is WasmBase.IR.TRUNC_SAT) {
         if (suffix_ir < EnumMembers!(WasmBase.IR_TRUNC_SAT).length) {
-            return cast(ExtendedIR)(ExtendedIR.I32_TRUNC_SAT_F32_S+suffix_ir);
+            return cast(ExtendedIR)(ExtendedIR.I32_TRUNC_SAT_F32_S + suffix_ir);
         }
         return ExtendedIR.ERROR;
     }
@@ -142,7 +145,6 @@ ExtendedIR convert(const WasmBase.IR ir, const ubyte suffix_ir) @safe pure nothr
     assert(cast(WasmBase.IR)(ExtendedIR.ERROR).convert is cast(ExtendedIR)(ubyte.max));
 }
 
-
 alias isWasmIR(ExtendedIR ir) = hasMember!(WasmBase.IR, ir.stringof);
 
 static unittest {
@@ -152,9 +154,8 @@ static unittest {
 
 shared static immutable(WasmBase.Instr[ExtendedIR]) instrExtTable;
 
-shared static this()
-{
-    foreach(ExtIR; EnumMembers!ExtendedIR) {
+shared static this() {
+    foreach (ExtIR; EnumMembers!ExtendedIR) {
         auto instr = ExtIR.convert in WasmBase.instrTable;
         if (instr) {
             instrExtTable[ExtIR] = *instr;
