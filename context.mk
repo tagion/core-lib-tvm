92f312fc3b478c355e91359660c4cd2415855184
Vlad Pazych <vladpazych@gmail.com>
2021-09-29 12:42:01 +0300

    WIP: exclude non-relevant directories

--- context.mk
+++ context.mk
@@ -1,8 +1,11 @@
 ifdef WASM_TEST
 SOURCE_FIND_EXCLUDE += "*/tagion/vm/wamr/*"
 else
+SOURCE_FIND_EXCLUDE += "*/scripts/*"
+SOURCE_FIND_EXCLUDE += "*/tvm/platform/*"
 SOURCE_FIND_EXCLUDE += "*/tests/*"
+SOURCE_FIND_EXCLUDE += "*/tests.deprected/*"
 endif
 
-libtagiontvm.ctx: 
+libtagiontvm.ctx: libtagionbasic.o libtagionwasm.o
 	@
