0b4361f7ea62e18948318c99d9e14c4fd71e4d65
cbleser <cr@tagion.org>
2021-09-30 17:07:17 +0300

    WIP: Unused scripts removed

--- scripts/wasm_runtime.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl -i.bak2
-while(<>) {
-    s/^(struct\s+WASMInterpFrame)/version(none)\n$1/;
-    print;
-}
--- scripts/wasm_runtime_common.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -i.bak2
-while(<>) {
-    s/^(bool\s+wasm_runtime_full_init)/version(none)\n$1/;
-    s/^(wasm_module_t\s+wasm_runtime_load\s+)/version(none)\n$1/;
-    s/^(void\s+wasm_runtime_destroy\s+\(\);)/version(none)\n$1/;
-    s/^(void\s+wasm_runtime_destroy_exec_env\s+)/version(none)\n$1/;
-    s/^(void\s+wasm_runtime_set_module_reader\s+)/version(none)\n$1/;
-    print;
-}
