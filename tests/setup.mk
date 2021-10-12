0b37e01043470f2a4b888df0e928a2aed9e414ea
cbleser <cr@tagion.org>
2021-10-12 12:26:48 +0300

    WASM samples  updated

--- tests/setup.mk
+++ tests/setup.mk
@@ -1,11 +1,13 @@
 #MAIN:=simple_alu
 
-DC?=ldc2
-LD:=/opt/wasi-sdk/bin/wasm-ld
+DCWASM?=ldc2
+LDWASM:=/opt/wasi-sdk/bin/wasm-ld
 
 WASM_TESTS+=simple_alu
 WASM_TESTS+=call_test
 WASM_TESTS+=simple_pay
+
+WASM_SAMPLES:=tests/wasm_samples.d
 
 WASMFLAGS+=-mtriple=wasm32-unknown-unknown-was
 WASMFLAGS+=--betterC
@@ -14,15 +16,17 @@ WASMFLAGS+=--Oz
 
 LDWFLAGS+=--allow-undefined
 
+WASM2DATA:=$(REPOROOT)/scripts/wasm2data.d
+
 define WASM_TEST
 WASMFILES+=$(BIN)/$1.wasm
 WASMSRC+=$(WASM_TEST_SRC)
 
 $(BIN)/$1.wasm: $(BIN)/$1.wo
-	$(LD) $(BIN)/$1.wo ${addprefix --export=, $(EXPORTS)} $(LDWFLAGS) -o $(BIN)/$1.wasm
+	$(LDWASM) $(BIN)/$1.wo ${addprefix --export=, $(EXPORTS)} $(LDWFLAGS) -o $(BIN)/$1.wasm
 
 $(BIN)/$1.wo: $1/$1.d
-	$(DC) $(WASMFLAGS) $(EXPRAFLAGS) -c $1/$1.d -of$(BIN)/$1.wo
+	$(DCWASM) $(WASMFLAGS) $(EXPRAFLAGS) -c $1/$1.d -of$(BIN)/$1.wo
 
 
 endef
